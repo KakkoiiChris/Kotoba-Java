83d4f20632d8140e287f6a7f6fba6541a9030aac
Christian Alexander <kakkoiichris@gmail.com>
2022-09-07 20:22:49 -0400

    Fully implemented the Glyph class and Glyph Effect class and all its subclasses.

--- src/kakkoiichris/kotoba/Font.java
+++ src/kakkoiichris/kotoba/Font.java
@@ -1,7 +1,137 @@
 package kakkoiichris.kotoba;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Font {
-    public Font(String path) {
+    private final int rows;
+    private final int cols;
+    private final int height;
     
+    private final char firstChar;
+    
+    private final CharacterInfo[] chars;
+    
+    public Font(String path) {
+        var data = new DataInputStream(new BufferedInputStream(Objects.requireNonNull(getClass().getResourceAsStream(path))));
+        
+        // Skip Format Bitmap Font File Version ID
+        try {
+            data.readShort();
+            
+            var fontImageWidth = fixed(data.readInt());
+            var fontImageHeight = fixed(data.readInt());
+            
+            var cellWidth = fixed(data.readInt());
+            var cellHeight = fixed(data.readInt());
+            
+            rows = fontImageHeight / cellHeight;
+            cols = fontImageWidth / cellWidth;
+            
+            height = cellHeight;
+            
+            // Skip bytes-per-pixel, always 8-bit grayscale
+            data.readByte();
+            
+            firstChar = (char) data.readUnsignedByte();
+            
+            var characterWidths = new int[256];
+            
+            for (var i = 0; i < characterWidths.length; i++) {
+                characterWidths[i] = data.readUnsignedByte();
+            }
+            
+            var allValues = new double[fontImageHeight][fontImageWidth];
+            
+            for (var value : allValues) {
+                for (var i = 0; i < value.length; i++) {
+                    value[i] = data.readUnsignedByte() / (double) 0xFF;
+                }
+            }
+            
+            var row = 0;
+            var col = 0;
+            
+            var charValues = new ArrayList<CharacterInfo>();
+            
+            for (var c = firstChar; c <= '\u00FF'; c++) {
+                var values = new ArrayList<Double>();
+                
+                var width = characterWidths[c];
+                
+                for (var y = 0; y < height; y++) {
+                    for (var x = 0; x < width; x++) {
+                        var vr = (row * cellHeight) + y;
+                        var vc = (col * cellWidth) + x;
+                        
+                        values.add(allValues[vr][vc]);
+                    }
+                }
+                
+                if (++col == cols) {
+                    col = 0;
+                    row++;
+                }
+                
+                charValues.add(new CharacterInfo(width, values.stream().mapToDouble(Double::doubleValue).toArray()));
+            }
+            
+            chars = (CharacterInfo[]) charValues.toArray();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private int fixed(int color) {
+        var byte0 = (color >> 24) & 0xFF;
+        var byte1 = (color >> 16) & 0xFF;
+        var byte2 = (color >> 8) & 0xFF;
+        var byte3 = color & 0xFF;
+        
+        return (byte3 << 24) | (byte2 << 16) | (byte1 << 8) | byte0;
+    }
+    
+    public CharacterInfo get(char c) {
+        return chars[((c >= firstChar) ? c : ' ') + firstChar];
+    }
+    
+    public int widthOfGlyphs(List<Glyph> glyphs, int space){
+        var fullWidth = 0;
+        
+        var iterator = glyphs.iterator();
+        
+        while (iterator.hasNext()) {
+            var c = iterator.next().getChar();
+            
+            var width = get(c).width;
+            
+            fullWidth += width + space;
+        }
+        
+        return fullWidth;
+    }
+    
+    public int widthOfChars(List<Character> chars, int space) {
+        var fullWidth = 0;
+        
+        var iterator = chars.iterator();
+        
+        while (iterator.hasNext()) {
+            var c = iterator.next();
+            
+            var width = get(c).width;
+            
+            fullWidth += width + space;
+        }
+        
+        return fullWidth;
+    }
+    
+    public record CharacterInfo(int width, double[] values) {
     }
 }
--- src/kakkoiichris/kotoba/Glyph.java
+++ src/kakkoiichris/kotoba/Glyph.java
@@ -1,4 +1,211 @@
 package kakkoiichris.kotoba;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.sin;
+
 public class Glyph {
+    private final char c;
+    private final Effect effect;
+    private final boolean inverted;
+    
+    private int color = 0;
+    private int offsetX = 0;
+    private int offsetY = 0;
+    
+    public Glyph(char c, Effect effect, boolean inverted) {
+        this.c = c;
+        this.effect = effect;
+        this.inverted = inverted;
+    }
+    
+    public char getChar() {
+        return c;
+    }
+    
+    public Effect getEffect() {
+        return effect;
+    }
+    
+    public boolean isInverted() {
+        return inverted;
+    }
+    
+    public int getColor() {
+        return color;
+    }
+    
+    public int getOffsetX() {
+        return offsetX;
+    }
+    
+    public int getOffsetY() {
+        return offsetY;
+    }
+    
+    public void update(double delta) {
+        effect.apply(this, delta);
+    }
+    
+    public interface Effect {
+        record Color(int rgb) implements Effect {
+            public static final Color red = new Color(0xF75DB3);
+            public static final Color orange = new Color(0xF9642D);
+            public static final Color yellow = new Color(0xFFF13A);
+            public static final Color green = new Color(0x6CB94B);
+            public static final Color blue = new Color(0x405AB9);
+            public static final Color purple = new Color(0xC76EDF);
+            public static final Color white = new Color(0xFFFFFF);
+            public static final Color silver = new Color(0xC4C4C4);
+            public static final Color gray = new Color(0x7A7A7A);
+            public static final Color soot = new Color(0x504D4B);
+            public static final Color black = new Color(0x2E2C2B);
+            
+            public static Color random() {
+                return new Color((int) (Math.random() * 0xFFFFFF));
+            }
+            
+            @Override
+            public void apply(Glyph glyph, double delta) {
+                glyph.color = rgb;
+            }
+            
+            @Override
+            public Effect copy() {
+                return new Color(rgb);
+            }
+        }
+        
+        class Cycle implements Effect {
+            private final double speed;
+            private final int[] colors;
+            
+            private double i = 0.0;
+            
+            public Cycle(double speed, int... colors) {
+                this.speed = speed;
+                this.colors = colors;
+            }
+            
+            @Override
+            public void apply(Glyph glyph, double delta) {
+                glyph.color = colors[(int) i % colors.length];
+                
+                i += speed * delta;
+            }
+            
+            @Override
+            public Effect copy() {
+                return new Cycle(speed, colors);
+            }
+        }
+        
+        class Random implements Effect {
+            private static Random instance;
+            
+            private Random() {
+            }
+            
+            public static Random get() {
+                if (instance == null) {
+                    instance = new Random();
+                }
+                
+                return instance;
+            }
+            
+            public void apply(Glyph glyph, double delta) {
+                glyph.color = (int) (Math.random() * 0xFFFFFF);
+            }
+            
+            public Effect copy() {
+                return instance;
+            }
+        }
+        
+        record Jitter(int x, int y) implements Effect {
+            @Override
+            public void apply(Glyph glyph, double delta) {
+                glyph.offsetX = (int) (Math.random() * ((x * 2) + 1)) - x;
+                glyph.offsetY = (int) (Math.random() * ((y * 2) + 1)) - y;
+            }
+            
+            @Override
+            public Effect copy() {
+                return new Jitter(x, y);
+            }
+        }
+        
+        static final class Wave implements Effect {
+            private final double amplitude;
+            private final double frequency;
+            private final double speed;
+            private final boolean vertical;
+            
+            public Wave(double amplitude, double frequency, double speed, boolean vertical) {
+                this.amplitude = amplitude;
+                this.frequency = frequency;
+                this.speed = speed;
+                this.vertical = vertical;
+            }
+            
+            private double phase = 0.0;
+            
+            @Override
+            public void apply(Glyph glyph, double delta) {
+                if (vertical) {
+                    glyph.offsetY = (int) (amplitude * sin(frequency*phase));
+                }
+                else {
+                    glyph.offsetX = (int) (amplitude * sin(frequency*phase));
+                }
+                
+                phase += speed * delta;
+            }
+            
+            @Override
+            public Effect copy() {
+                return new Wave(amplitude, frequency, speed, vertical);
+            }
+        }
+        
+        class Multi implements Effect {
+            private final List<Effect> effects = new ArrayList<>();
+            
+            public Multi(Effect... effects) {
+                this.effects.addAll(List.of(effects));
+            }
+            
+            @Override
+            public Multi and(Effect effect) {
+                if (effect instanceof Multi multi) {
+                    effects.addAll(multi.effects);
+                }
+                else {
+                    effects.add(effect);
+                }
+                
+                return this;
+            }
+            
+            @Override
+            public void apply(Glyph glyph, double delta) {
+                effects.forEach(effect -> effect.apply(glyph, delta));
+            }
+            
+            @Override
+            public Effect copy() {
+                return new Multi((Effect[]) effects.stream().map(Effect::copy).toArray());
+            }
+        }
+        
+        default Multi and(Effect effect) {
+            return new Multi(this, effect);
+        }
+        
+        void apply(Glyph glyph, double delta);
+        
+        Effect copy();
+    }
 }
